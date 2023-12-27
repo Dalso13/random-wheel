05122062235c0e6b2622d273fac1d070e065f336
Dalso <flskwhdhk@gmail.com>
2023-12-27 15:32:21 +0900

    배포 준비 및 자잘한 수정

--- lib/random_wheel.dart
+++ lib/random_wheel.dart
@@ -35,87 +35,90 @@ class _RandomWheelState extends State<RandomWheel> {
         ),
       ),
       body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 100,
-              child: _result != null
-                  ? Column(
-                      children: [
-                        Text(
-                          '당첨!',
-                          style: TextStyle(
-                              fontSize: 30,
-                              color: Colors.green[700],
-                              fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          _result!,
-                          style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.green[400],
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    )
-                  : Text('돌림판을 돌려주세요',
-                      style: TextStyle(
-                          fontSize: 20,
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold)),
-            ),
-            SizedBox(
-              width: 350,
-              height: 350,
-              child: FortuneWheel(
-                  physics: NoPanPhysics(),
-                  animateFirst: false,
-                  selected: _controller.stream,
-                  onAnimationEnd: () {
-                    setState(() {
-                      _result = widget.items[_controller.value];
-                    });
-                  },
-                  items: widget.items
-                      .map((e) => FortuneItem(
-                              child: SizedBox(
-                            width: 100,
-                            child: Text(
-                              e,
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 1,
-                              style: TextStyle(fontSize: 20),
-                            ),
-                          )))
-                      .toList()),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                      onPressed: () {
-                        setState(() {
-                          _controller
-                              .add(Fortune.randomInt(0, widget.items.length));
-                        });
-                      },
-                      child: const Text("돌리기")),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 16),
-                    child: ElevatedButton(
-                        onPressed: () {
-                          context.pop();
-                        },
-                        child: const Text("다시설정")),
-                  ),
-                ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 100,
+                child: _result != null
+                    ? Column(
+                        children: [
+                          Text(
+                            '당첨!',
+                            style: TextStyle(
+                                fontSize: 30,
+                                color: Colors.green[700],
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            _result!,
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.green[400],
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      )
+                    : const Text('돌림판을 돌려주세요',
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold)),
               ),
-            ),
-          ],
+              SizedBox(
+                width: 350,
+                height: 350,
+                child: FortuneWheel(
+                    physics: NoPanPhysics(),
+                    animateFirst: false,
+                    selected: _controller.stream,
+                    onAnimationEnd: () {
+                      setState(() {
+                        _result = widget.items[_controller.value];
+                      });
+                    },
+                    items: widget.items
+                        .map((e) => FortuneItem(
+                                child: SizedBox(
+                              width: 100,
+                              child: Text(
+                                textAlign: TextAlign.center,
+                                e,
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 1,
+                                style: const TextStyle(fontSize: 20),
+                              ),
+                            )))
+                        .toList()),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            _controller
+                                .add(Fortune.randomInt(0, widget.items.length));
+                          });
+                        },
+                        child: const Text("돌리기")),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16),
+                      child: ElevatedButton(
+                          onPressed: () {
+                            context.pop();
+                          },
+                          child: const Text("다시설정")),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
