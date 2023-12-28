e3801fc441e525aa1dedd7f74f718aafa03e3bd3
Dalso <flskwhdhk@gmail.com>
2023-12-28 14:14:43 +0900

    배포 준비 및 자잘한 수정

--- lib/random_wheel_main.dart
+++ lib/random_wheel_main.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
@@ -25,62 +23,62 @@ class _RandomWheelMainState extends State<RandomWheelMain> {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Column(
               mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                const Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text('돌려 돌려 돌림판', style: TextStyle(fontSize: 30)),
-                    Text('(2개~8개 설정가능)', style: TextStyle(color: Colors.grey)),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 32),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        width: 250,
-                        height: 70,
-                        child: TextField(
-                          controller: textController,
-                          decoration: InputDecoration(
-                            hintText: '옵션 추가',
-                            filled: true,
-                            fillColor: Colors.green[50],
-                          ),
-                          maxLength: 20,
-                        ),
+                Text('돌려 돌려 돌림판', style: TextStyle(fontSize: 30)),
+                Text('(2개~8개 설정가능)', style: TextStyle(color: Colors.grey)),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 32),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 250,
+                    height: 70,
+                    child: TextField(
+                      controller: textController,
+                      decoration: InputDecoration(
+                        hintText: '옵션 추가',
+                        filled: true,
+                        fillColor: Colors.green[50],
                       ),
-                      IconButton(
-                        onPressed: () {
-                          setState(() {
-                            if (textController.text.length > 0 &&
-                                values.length < 8 && textController.text.trim() != '') {
-                              values.add(textController.text);
-                              textController.text = '';
-                            }
-                          });
-                        },
-                        icon: const Icon(
-                          Icons.add,
-                        ),
-                      )
-                    ],
+                      maxLength: 20,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  width: 200,
-                  height: 250,
-                  child: ListView(
-                    children: values
-                        .map((e) =>
-                        Row(
+                  IconButton(
+                    onPressed: () {
+                      setState(() {
+                        if (textController.text.length > 0 &&
+                            values.length < 8 &&
+                            textController.text.trim() != '') {
+                          values.add(textController.text);
+                          textController.text = '';
+                        }
+                      });
+                    },
+                    icon: const Icon(
+                      Icons.add,
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              width: 200,
+              height: 250,
+              child: ListView(
+                children: values
+                    .map((e) => Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
                             SizedBox(
@@ -88,6 +86,7 @@ class _RandomWheelMainState extends State<RandomWheelMain> {
                                 child: Text(
                                   e,
                                   overflow: TextOverflow.ellipsis,
+                                  style: const TextStyle(fontSize: 20),
                                 )),
                             SizedBox(
                                 height: 25,
@@ -100,32 +99,31 @@ class _RandomWheelMainState extends State<RandomWheelMain> {
                                     icon: const Icon(Icons.remove)))
                           ],
                         ))
-                        .toList(),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 8),
-                  child: Text('${values.length}개 입력',
-                      style: TextStyle(
-                          fontSize: 20,
-                          color: values.length < 2 ? Colors.red : Colors
-                              .green)),
-                ),
-                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                  ElevatedButton(
-                      onPressed: () {
-                        if (values.length > 1 && values.length < 9) {
-                          context.push(Uri(
+                    .toList(),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 8, top: 16),
+              child: Text('${values.length}개 입력',
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: values.length < 2 ? Colors.red : Colors.green)),
+            ),
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              ElevatedButton(
+                  onPressed: () {
+                    if (values.length > 1 && values.length < 9) {
+                      context.push(Uri(
                               path: '/wheel',
                               queryParameters: {'items': values.join(',')})
-                              .toString());
-                        }
-                      },
-                      child: const Text("설정완료")),
-                ])
-              ],
-            ),
-          ),
-        ));
+                          .toString());
+                    }
+                  },
+                  child: const Text("설정완료")),
+            ])
+          ],
+        ),
+      ),
+    ));
   }
 }
